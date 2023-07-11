bb10d9c47a6d1489c0d6f8910ca839b2f11ce958
Abass Success <118232578+GoodnessAbass19@users.noreply.github.com>
2023-07-12 00:23:59 +0100

    className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="bg-gray-900 py-4">
-      <div class="container mx-auto px-4 flex items-center justify-between">
-        <p class="text-gray-300 text-sm">
+    <footer className="bg-gray-900 py-4">
+      <div className="container mx-auto px-4 flex items-center justify-between">
+        <p className="text-gray-300 text-sm">
           Challenge by{" "}
           <a href="https://www.frontendmentor.io/">Frontend Mentor</a>
         </p>
-        <p class="text-gray-300 text-sm">
+        <p className="text-gray-300 text-sm">
           Coded by{" "}
           <a href="https://goodness-dev-portfolio.vercel.app/">Goodness Dev</a>
         </p>
